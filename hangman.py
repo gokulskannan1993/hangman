27310ebeec888a78b982da58cc7e1260ff33889e
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 09:29:19 +0100

    no change

--- hangman.py
+++ hangman.py
@@ -3,8 +3,12 @@ import requests, random
 
 
 # Generate a random word
-
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
+
 answer = random.choice(WORDS)
+
+
+
+
