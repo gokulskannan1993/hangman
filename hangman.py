8247a556d9c7e53ec693bc6692e72b4a718976ee
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 10:49:37 +0100

    more chars added

--- hangman.py
+++ hangman.py
@@ -8,10 +8,9 @@ game_over = False
 
 
 # Generate a random word
-WORDS = ["ironman", "captainamerica", "spiderman", "hawkeye", "blackwidow", "doctorstrange", "antman", "thor", "loki","blackpanther","killmonger","ultron","thanos","starlord","drax","gamora","rocket","groot","mantis","yellowjacket","shuri","tonystark","mandarin","vulture","daredevil","captainmarvel","msmarvel","hulk","wasp","brucebanner","scottlang",]
+WORDS = ["ironman", "captainamerica", "spiderman", "hawkeye", "blackwidow", "doctorstrange", "antman", "thor", "loki","blackpanther","killmonger","ultron","thanos","starlord","drax","gamora","rocket","groot","mantis","yellowjacket","shuri","tonystark","mandarin","vulture","daredevil","captainmarvel","msmarvel","hulk","wasp","brucebanner","scottlang","pepperpotts","caroldanvers","peterparker","tchalla","mbaku","scarletwitch","wanda","vision","jarvis","edith", "auntmay", "happyhogan",]
 
 answer = random.choice(WORDS).upper()
-print(answer)
 
 
 # Create blank array
