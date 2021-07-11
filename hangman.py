75047ae45904351fda3682ff1df81248de7b27cd
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 09:43:24 +0100

    asks for input and guess is checked with display

--- hangman.py
+++ hangman.py
@@ -7,8 +7,26 @@ word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 
-answer = random.choice(WORDS)
+answer = random.choice(WORDS).upper()
 
+
+# Create blank array
+display = []
+for letter in answer:
+    display += "_"
+
+
+
+
+# Ask for user input
+guess = input("Guess a letter: ").upper()
+
+
+
+for position in range(0, len(answer)):
+    letter = answer[position]
+    if(guess == letter):
+        display[position] = letter
 
 
 
