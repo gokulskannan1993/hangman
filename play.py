d303fcce3ae3590d2ff7849f158a90ab8e6c7306
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 11:09:38 +0100

    game almost done

--- play.py
+++ play.py
@@ -1,9 +1,10 @@
-from resources import stages, WORDS
+from resources import stages, WORDS, won, lost
 
 
 import random
 
 game_over = False
+lives = 6
 
 
 
@@ -19,25 +20,40 @@ for letter in answer:
 
 
 
-
+# For running in loop
 while not game_over:
+
+    print(stages[lives])
+
+
     # Ask for user input
     guess = input("Guess a letter: ").upper()
 
+    
 
 
-
-
+    # Checks for the match
     for position in range(0, len(answer)):
         letter = answer[position]
-        print(f"Current position is {letter} and guess is {guess}")
         if(guess == letter):
             display[position] = letter
+    
+    
+    # Check for wrong guess
+    if guess not in answer:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print(stages[lives])
+            print(lost)
 
+    # Print the display each time
+    print(f"{' '.join(display)}")
 
-    print(display)
+    # If the player wins
     if "_" not in display:
         game_over = True
-        print("Your win")
+        print(won)
+    
 
 
--- resources.py
+++ resources.py
@@ -1,5 +1,22 @@
 WORDS = ["ironman", "captainamerica", "spiderman", "hawkeye", "blackwidow", "doctorstrange", "antman", "thor", "loki","blackpanther","killmonger","ultron","thanos","starlord","drax","gamora","rocket","groot","mantis","yellowjacket","shuri","tonystark","mandarin","vulture","daredevil","captainmarvel","msmarvel","hulk","wasp","brucebanner","scottlang","pepperpotts","caroldanvers","peterparker","tchalla","mbaku","scarletwitch","wanda","vision","jarvis","edith", "auntmay", "happyhogan",]
 
+won = '''
+
+
+=========
+YOU WON
+=========
+
+'''
+
+lost = '''
+
+
+=========
+YOU LOST
+=========
+
+'''
 
 
 
